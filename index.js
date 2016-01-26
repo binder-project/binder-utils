5fa556c793525b2bdc8eac01337990273123a899
Andrew Osheroff <andrewosh@gmail.com>
2016-01-26 15:13:48 -0500

    Added startWithPM2

---
+++ index.js
@@ -0,0 +1,18 @@
+var pm2 = require('pm2')
+
+module.exports = {
+  startWithPM2: function (app) {
+    pm2.connect(function (err) {
+      if (err) {
+        console.error(err)
+        process.exit(2)
+      }
+      pm2.start(app, function (err, apps) {
+        if (err) {
+          console.error(err)
+        }
+        pm2.disconnect()
+      })
+    })
+  }
+}
